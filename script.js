f0b5877711833fcf76796a313d6ff1fc6feaaf78
Denis Denisov <deniskrukovz@gmail.com>
2023-01-29 17:57:30 +0700

    Реузьтат урока lesson02

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'myProj';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 999;
+let rollback = 50;
+let fullPrice = 257800;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта' + ' ' +  fullPrice + ' ' + 'рублей/ долларов/гривен/юани');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log((fullPrice * (rollback/100)));
+
+
 
 alert('Hello world!');
 
