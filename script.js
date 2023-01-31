ed0ee7fab6b6b4a7fda6023b9e40dd977d8ee50e
Denis Denisov <deniskrukovz@gmail.com>
2023-02-01 00:57:09 +0700

    Результат урока Lesson03

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-let title = 'myProj';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 999;
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = parseInt(prompt('Сколько будет стоить данная работа?'));
+
 let rollback = 50;
-let fullPrice = 257800;
-let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/ долларов/гривен/юани');
-console.log('Стоимость разработки сайта' + ' ' +  fullPrice + ' ' + 'рублей/ долларов/гривен/юани');
+let adaptive = prompt('Нужен ли адаптив на сайте?');
+if (adaptive == 'Да'){
+    adaptive = true;
+} else {
+    adaptive = false;
+}
 
-console.log(screens.toLowerCase().split(', '));
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = parseInt(prompt('Сколько это будет стоить?'));
 
-console.log((fullPrice * (rollback/100)));
+let service2 = parseInt(prompt('Какой дополнительный тип услуги нужен?'));
+let servicePrice2 = parseInt(prompt('Сколько это будет стоить?'));
+
+let fullPrice = parseInt(screenPrice + servicePrice1 + servicePrice2);
+
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000){
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000){
+    console.log('Даем скидку в 5%');
+} else if (fullPrice < 15000 && fullPrice > 0){
+    console.log('Скидка не предусмотрена');
+} else if(fullPrice <= 0){
+    console.log('Что то пошло не так');
+}
+
+
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
+// console.log(screens.length);
+// console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/ долларов/гривен/юани');
+// console.log('Стоимость разработки сайта' + ' ' +  fullPrice + ' ' + 'рублей/ долларов/гривен/юани');
+
+// console.log(screens.toLowerCase().split(', '));
+
+// console.log((fullPrice * (rollback/100)));
 
 
 
-alert('Hello world!');
+// alert('Hello world!');
 
-console.log('Hello space!');
+// console.log('Hello space!');
